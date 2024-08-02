70d7f185f3eeecb794e6da4d5368474943fe14e8
lalalaoalla <avologina93@mail.ru>
2024-08-02 17:59:57 +0700

    Модели продуктов
    Добавила и зарегистрировала две модели - категории и продукты

--- tencho/products/admin.py
+++ tencho/products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class CategoryProductAdmin(admin.ModelAdmin):
+    list_display=['name']
+
+    class Meta:
+        model= CategoryProducts
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['name','cost', 'category']
+    list_filter=['name', 'category',]
+    search_fields=['name', 'category']
+
+    class Meta:
+        model=Product
+
+admin.site.register(CategoryProducts,CategoryProductAdmin)
+admin.site.register(Product,ProductAdmin)
--- tencho/products/models.py
+++ tencho/products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class CategoryProducts(models.Model):
+    name=models.CharField(max_length=128,unique=True)
+
+    class Meta: 
+        verbose_name='Категория'
+        verbose_name_plural='Категории'
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    #ВНИМАНИЕ on_delete указывает, что произойдет, если поле КАТЕГОРИЯ будет удалено
+    name=models.CharField(max_length=128)
+    cost=models.DecimalField(max_digits=10,decimal_places=2)
+    category=models.ForeignKey(CategoryProducts,on_delete=models.CASCADE)#тоже будет удален
+    description=models.TextField()
+    image=models.ImageField()
+    is_active=models.BooleanField(default=True)
+    created=models.DateTimeField()
+    updated=models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+    
+    def __str__(self):
+        return '%s %s %s' % (self.category, self.name, self.cost)
+
