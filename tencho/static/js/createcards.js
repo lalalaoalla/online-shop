a36a83ff628009851a1abde3dfd3733d3543f0da
lalalaoalla <avologina93@mail.ru>
2024-08-05 09:55:49 +0700

    Переход по ссылке купить на карточке в index

--- tencho/static/js/createcards.js
+++ tencho/static/js/createcards.js
@@ -21,7 +21,7 @@ function loadpr() {
             out+='<div class="underline"></div>'
             out+='<p class="item-title">'+data[key].name+'</p>'
             out+='<p class="price">Цена: '+data[key].cost+'₽</p>'
-            out+='<a href="product.html" onclick="buy(this)" data-name="'+data[key].name+'" data-cost="'+data[key].cost+'"  data-img="'+data[key].image+'" data-desc="'+data[key].description+'" data-category="'+data[key].category+'">Купить</a>'
+            out+='<a href="/product_detail/'+data[key].id+'" onclick="buy(this)" data-name="'+data[key].name+'" data-cost="'+data[key].cost+'"  data-img="'+data[key].image+'" data-desc="'+data[key].description+'" data-category="'+data[key].category+'">Купить</a>'
             out+='</div>'
             out+='</div>'
           }
@@ -46,7 +46,7 @@ function loadnew() {
             out+='<div class="slider-info">'
             out+='<h1 class="slider-item-title">'+data[key].name+'</h1>'
             out+='<p class="price">Цена: '+data[key].cost+'₽</p>'
-            out+='<a class="slider-buy-btn" href="product.html" onclick="buy(this)" data-name="'+data[key].name+'" data-cost="'+data[key].cost+'"  data-img="'+data[key].image+'" data-desc="'+data[key].description+'" data-category="'+data[key].category+'">Купить</a>'
+            out+='<a class="slider-buy-btn" href="/product_detail/'+data[key].id+'" onclick="buy(this)" data-name="'+data[key].name+'" data-cost="'+data[key].cost+'"  data-img="'+data[key].image+'" data-desc="'+data[key].description+'" data-category="'+data[key].category+'">Купить</a>'
             out+='</div>'
             out+='</div>'
             out+='</div>'
