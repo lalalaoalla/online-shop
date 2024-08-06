6dad0ba246d5d73e1dd983a46554ed9469e8fd4f
lalalaoalla <avologina93@mail.ru>
2024-08-06 11:14:14 +0700

    Update admin.py
    Регистрация модели пользователей в нашем приложении

--- tencho/users/admin.py
+++ tencho/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    #list_display=['first_name','last_name', 'username', 'email']
+    search_fields=['first_name', 'username']
+
+    class Meta:
+        model = User
+
+admin.site.register(User,UserAdmin)
