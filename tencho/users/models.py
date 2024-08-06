725f849fc548f9911d32ace4ee4554f3d9a6861a
lalalaoalla <avologina93@mail.ru>
2024-08-06 11:13:53 +0700

    Update models.py
    Переопределила модель пользователя немного(потом если что еще поменяем)

--- tencho/users/models.py
+++ tencho/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_avatar/', null=True, blank=True)
+
+    class Meta:
+        verbose_name='Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
