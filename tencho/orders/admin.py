aa90c2bacc52f0b18a25a161bef20750452bf99f
lalalaoalla <avologina93@mail.ru>
2024-08-07 11:09:54 +0700

    Модель статусов заказа

--- tencho/orders/admin.py
+++ tencho/orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Status
 # Register your models here.
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+    class Meta:
+        model = Status
+
+admin.site.register(Status, StatusAdmin)
+
+
--- tencho/orders/models.py
+++ tencho/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Status(models.Model):
+    '''Статусы нашего заказа'''
+    name = models.CharField(max_length=16)
+    is_avtive = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)#дата добавления заказа
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.name
+    class Meta:
+        verbose_name='Статус заказа'
+        verbose_name_plural = 'Статусы заказа'
